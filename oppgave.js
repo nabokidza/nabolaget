161f5e79417040c10d7dca5356b693ad292286ba
nabokidsa <131347177+nabokidsa@users.noreply.github.com>
2023-04-20 22:15:54 +0200

    Update oppgave.js

--- oppgave.js
+++ oppgave.js
@@ -1,8 +1,13 @@
 
 const type = "rosa";
 const oppgaver = [
-    {id:1, beskrivelse:"Hopp hvor mange år du er", icon:"🪂", lyd:"oppgave1.m4a" },
+    {id:1, beskrivelse:"Hopp hvor mange år du er", icon:"🪂", lyd:"oppgave1.m4a" }
     {id:2, beskrivelse:"Vis din kuleste dans", icon:"💃", lyd:"oppgave2.m4a" }
+    {id:3, beskrivelse:"Vis din rareste grimase", icon:"?", lyd:"oppgave3.m4a" }
+    {id:4, beskrivelse:"Brøl som en løve", icon:"?", lyd:"oppgave4.m4a" }
+    {id:5, beskrivelse:"Slå deg på brystet som en gorilla", icon:"?", lyd:"oppgave5.m4a" }
+ 
+    
 ]
 
 const oppgaveBeskrivelse = document.getElementById("oppgave");
