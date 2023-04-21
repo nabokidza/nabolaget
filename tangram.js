4821f6096b82b9e8d0f2e4608a460af86a12f373
nabokidsa <131347177+nabokidsa@users.noreply.github.com>
2023-04-21 10:54:09 +0200

    Update tangram.js

--- tangram.js
+++ tangram.js
@@ -3,15 +3,15 @@ const type = "gul";
 const oppgaver = [
     {id:1, losning:"losning.png", 
     biter:[
-        { koordinater:"133,365,362,368,247,495,7,495", riktigBilde="001.jpg", form="poly"},
-        { koordinater:"38,0,496,7,249,251,265,19,233,162", riktigBilde="002.jpg", form="poly"},
-        { koordinater:"105,1506,228,1383,460,1383,340,1511", riktigBilde="004.jpg", form="poly"},
-        { koordinater:"98,1017,593,1022,346,1269", riktigBilde="008.jpg", form="poly"}, 
-        { koordinater:"232,1379,459,1379,344,1271", riktigBilde="006.jpg", form="poly"}, 
-        { koordinater:"352,1267,463,1379,591,1249,482,1135", riktigBilde="001.jpg", form="poly"},
-        { koordinater:"486,1133,592,1023,591,1240", riktigBilde="005.jpg", form="poly"},
-        { koordinater:"346,1506,593,1252,595,1509", riktigBilde="003.jpg", form="poly"},
-        { koordinater:"104,1024,102,1503,344,1264", riktigBilde="007.jpg", form="poly"},
+        { koordinater:"133,365,362,368,247,495,7,495", riktigBilde="001.png", form="poly"},
+        { koordinater:"38,0,496,7,249,251,265,19,233,162", riktigBilde="002.png", form="poly"},
+        { koordinater:"105,1506,228,1383,460,1383,340,1511", riktigBilde="004.png", form="poly"},
+        { koordinater:"98,1017,593,1022,346,1269", riktigBilde="008.png", form="poly"}, 
+        { koordinater:"232,1379,459,1379,344,1271", riktigBilde="006.png", form="poly"}, 
+        { koordinater:"352,1267,463,1379,591,1249,482,1135", riktigBilde="001.png", form="poly"},
+        { koordinater:"486,1133,592,1023,591,1240", riktigBilde="005.png", form="poly"},
+        { koordinater:"346,1506,593,1252,595,1509", riktigBilde="003.png", form="poly"},
+        { koordinater:"104,1024,102,1503,344,1264", riktigBilde="007.png", form="poly"},
         
         
       
@@ -21,26 +21,26 @@ const oppgaver = [
     
         id:2, losning:"102.png",
         biter:[
-            { koordinater:"942,847,775,1014,952,1009", riktigBilde="107.jpg", form="poly"},
-            { koordinater:"950,846,1144,846,1138,1005,958,100", riktigBilde="106.jpg", form="poly"},
-            { koordinater:"954,1012,1136,1012,964,1193,783,1188", riktigBilde="103.jpg", form="poly"},
-            { koordinater:"789,1195,960,1193,787,1353", riktigBilde="104.jpg", form="poly"},
-            { koordinater:"973,1191,786,1363,971,1553", riktigBilde="108.jpg", form="poly"}, 
-            { koordinater:"979,1205,979,1549,1330,1548", riktigBilde="101.jpg", form="poly"}, 
-            { koordinater:"980,1192,1471,1197,1227,1440", riktigBilde="105.jpg", form="poly"},
+            { koordinater:"942,847,775,1014,952,1009", riktigBilde="107.png", form="poly"},
+            { koordinater:"950,846,1144,846,1138,1005,958,100", riktigBilde="106.png", form="poly"},
+            { koordinater:"954,1012,1136,1012,964,1193,783,1188", riktigBilde="103.png", form="poly"},
+            { koordinater:"789,1195,960,1193,787,1353", riktigBilde="104.png", form="poly"},
+            { koordinater:"973,1191,786,1363,971,1553", riktigBilde="108.png", form="poly"}, 
+            { koordinater:"979,1205,979,1549,1330,1548", riktigBilde="101.png", form="poly"}, 
+            { koordinater:"980,1192,1471,1197,1227,1440", riktigBilde="105.png", form="poly"},
             
             
     ] },
     {
         id:3, losning:"206.png",
         biter:[
-            { koordinater:"1100,559,973,690,1090,807,1230,666,1223,666", riktigBilde="205.jpg", form="poly"},
-            { koordinater:"846,684,650,869,1014,881", riktigBilde="201.jpg", form="poly"},
-            { koordinater:"1159,872,1313,696,1497,682,1340,870", riktigBilde="2014.jpg", form="poly"},
-            { koordinater:"844,1058,1340,1054,1089,808", riktigBilde="208.jpg", form="poly"},
-            { koordinater:"846,1063,1337,1062,1089,1310", riktigBilde="202.jpg", form="poly"},
-            { koordinater:"842,1264,981,1211,903,1411", riktigBilde="207.jpg", form="poly"},
-            { koordinater:"1200,1205,1353,1258,1303,1402", riktigBilde="203.jpg", form="poly"},
+            { koordinater:"1100,559,973,690,1090,807,1230,666,1223,666", riktigBilde="205.png", form="poly"},
+            { koordinater:"846,684,650,869,1014,881", riktigBilde="201.png", form="poly"},
+            { koordinater:"1159,872,1313,696,1497,682,1340,870", riktigBilde="2014.png", form="poly"},
+            { koordinater:"844,1058,1340,1054,1089,808", riktigBilde="208.png", form="poly"},
+            { koordinater:"846,1063,1337,1062,1089,1310", riktigBilde="202.png", form="poly"},
+            { koordinater:"842,1264,981,1211,903,1411", riktigBilde="207.png", form="poly"},
+            { koordinater:"1200,1205,1353,1258,1303,1402", riktigBilde="203.png", form="poly"},
             
    ] }, 
    { 
