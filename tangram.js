97932f0ad4bd49f3805ba3813a917a89c1fd4086
Christian Simonsen <crsdev@gmail.com>
2023-04-21 09:19:50 +0200

    begynelse av tangram oppgaven

---
+++ tangram.js
@@ -0,0 +1,9 @@
+
+const type = "gul";
+const oppgaver = [
+    {id:1, losning:"losning.png", 
+    biter:[
+        { koordinater:"133,365,362,368,247,495,7,495", riktigBilde="001.jpg", form="poly"},
+        { koordinater:"38,0,496,7,249,251,265,19,233,162", riktigBilde="002.jpg", form="poly"},
+    ] }
+]
