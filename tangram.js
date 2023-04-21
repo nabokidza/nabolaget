6dc701366638c0d4793156174b7f898e8fc729fd
nabokidsa <131347177+nabokidsa@users.noreply.github.com>
2023-04-21 10:15:54 +0200

    Update tangram.js

--- tangram.js
+++ tangram.js
@@ -5,5 +5,20 @@ const oppgaver = [
     biter:[
         { koordinater:"133,365,362,368,247,495,7,495", riktigBilde="001.jpg", form="poly"},
         { koordinater:"38,0,496,7,249,251,265,19,233,162", riktigBilde="002.jpg", form="poly"},
-    ] }
+        { koordinater:"105,1506,228,1383,460,1383,340,1511", riktigBilde="004.jpg", form="poly"},
+        { koordinater:"98,1017,593,1022,346,1269", riktigBilde="008.jpg", form="poly"}, 
+        { koordinater:"232,1379,459,1379,344,1271", riktigBilde="006.jpg", form="poly"}, 
+        { koordinater:"352,1267,463,1379,591,1249,482,1135", riktigBilde="001.jpg", form="poly"},
+        { koordinater:"486,1133,592,1023,591,1240", riktigBilde="005.jpg", form="poly"},
+        { koordinater:"346,1506,593,1252,595,1509", riktigBilde="003.jpg", form="poly"},
+        { koordinater:"104,1024,102,1503,344,1264", riktigBilde="007.jpg", form="poly"},
+        
+        
+      
+      
+    ] },
+    {
+    
+        id:2, losning:"102.png",
+    }
 ]
