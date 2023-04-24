ff23690c34317bd6ab3d4f80a5f9fdcee9475fd7
nabokidsa <131347177+nabokidsa@users.noreply.github.com>
2023-04-24 19:08:39 +0200

    Update rimer.js

--- rimer.js
+++ rimer.js
@@ -2,8 +2,8 @@
 const type = "gronn";
 const rimeoppgaver = [
     {id:"oppgave1", riktig:"mus.jpg", riktigLyd:"riktig1.mp3", fokus:"hus.jpg", feil:["elefant.jpg","melk.jpg","vindmolle.jpg"], beskrivelse:"Hva rimer på hus?" , lyd:"oppgave4.m4a"},
-    {id:"oppgave2", riktig:"kattriktig.jpg", riktiglyd:"riktig.mp3", fokus:"hattriktig.jpg", feil:["feil11.jpg", "feil12.jpg", "feil13.jpg"], beskrivelse:"Hva rimer på hatt?" , lyd:"oppgave3.m4a"},
-    {id:"oppgave3", riktig:"stolriktig.jpg", riktiglyd:"riktig", fokus:"solriktig.jpg", feil:["feil20.jpg", "feil21.jpg", "feil22.jpg"], beskrivelse:"Hva rimer på sol?" , lyd:"?"}
+    {id:"oppgave2", riktig:"kattriktig.jpg", riktiglyd:"riktig1.mp3", fokus:"hattriktig.jpg", feil:["feil11.jpg", "feil12.jpg", "feil13.jpg"], beskrivelse:"Hva rimer på hatt?" , lyd:"oppgave3.m4a"},
+    {id:"oppgave3", riktig:"stolriktig.jpg", riktiglyd:"riktig1.mp3", fokus:"solriktig.jpg", feil:["feil20.jpg", "feil21.jpg", "feil22.jpg"], beskrivelse:"Hva rimer på sol?" , lyd:"oppgave2.m4a"},
     
 ]
 
